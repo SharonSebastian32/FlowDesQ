6dc0855589a6c014d1fd48fa4108427ca924ff1d
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-03-14 14:08:26 +0530

    feat: Sidebar added

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Home.css";
+import SideBar from "../SideBar/SideBar";
+function Home() {
+  return (
+    <div id="home">
+      <SideBar />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { IoMenuSharp } from "react-icons/io5";
+import { GrAppsRounded } from "react-icons/gr";
+import { TbClock2 } from "react-icons/tb";
+import { BsFillPersonFill } from "react-icons/bs";
+import { MdGroups } from "react-icons/md";
+import { PiImageLight } from "react-icons/pi";
+import { RiQuestionnaireLine } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+
+function SideBar() {
+  return (
+    <div id="sidebar">
+      <div className="menuIcons">
+        <IoMenuSharp id="menu" />
+        <div className="menu-groups">
+          <GrAppsRounded id="apps" />
+          <TbClock2 id="clock" />
+          <BsFillPersonFill id="person" />
+          <MdGroups id="groups" />
+          <PiImageLight id="image" />
+          <RiQuestionnaireLine id="question" />
+          <IoSettingsOutline id="settings" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
