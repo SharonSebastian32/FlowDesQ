cedfd8768306c6e48b4909382086c10dba9b5e85
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-03-17 23:41:55 +0530

    4/2

--- src/Components/Widget/SectionTwo.jsx
+++ src/Components/Widget/SectionTwo.jsx
@@ -6,9 +6,57 @@ function SectionTwo() {
       {/* total workign time doughnut chart */}
       <div className="comp working">Working Time</div>
       {/*   Productivity, Absent, Arrived, Late */}
-      <div className="comp main4Components">4 Components</div>
+      <div
+        className="comp main4Components"
+        style={{
+          color: "white",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gridTemplateRows: "1fr 1fr",
+          gap: "10px",
+          marginLeft: "3px",
+          marginRight: "3px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            borderRadius: "10px",
+          }}
+        >
+          1
+        </div>
+        <div
+          style={{
+            backgroundColor: "orange",
+            color: "white",
+            borderRadius: "10px",
+          }}
+        >
+          1
+        </div>
+        <div
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            borderRadius: "10px",
+          }}
+        >
+          1
+        </div>
+        <div
+          style={{
+            backgroundColor: "pink",
+            color: "white",
+            borderRadius: "10px",
+          }}
+        >
+          1
+        </div>
+      </div>
       {/* Productive, Slacking */}
-      <div className="comp 2Components">2 Components</div>
+      <div className="comp twoComponents">2 Components</div>
     </div>
   );
 }
