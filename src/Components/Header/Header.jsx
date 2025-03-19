56d15b80d6f8cd3883a587b904cbb1c4f4b37672
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-03-19 22:23:37 +0530

    feat: update Header component layout and styling for improved alignment and readability

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,29 @@ import "./Header.scss";
 function Header() {
   return (
     <div>
-      <div className="header">
+      <div
+        className="header"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
         Logo Global-Search Admin Info messages notifications
       </div>
       <div className="line-seperator"></div>
-      <div className="sub-header">Dashboard Text and Calendar</div>
+      <div
+        className="sub-header"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        <span>Dashboard Text and Calendar</span>
+      </div>
     </div>
   );
 }
