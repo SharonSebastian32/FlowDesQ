4baa9b38056cd2e647d66801969c0f1341844615
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-03-20 10:28:07 +0530

    sub-header completed✔️

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { GoChevronLeft } from "react-icons/go";
+import { GoChevronRight } from "react-icons/go";
+import { FaCalendarDays } from "react-icons/fa6";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+
 import "./Header.scss";
 function Header() {
   return (
@@ -15,16 +20,20 @@ function Header() {
         Logo Global-Search Admin Info messages notifications
       </div>
       <div className="line-seperator"></div>
-      <div
-        className="sub-header"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          padding: "20px",
-        }}
-      >
-        <span>Dashboard Text and Calendar</span>
+      <div className="sub-header">
+        <div className="text-dash">Dashboard</div>
+        <div id="calendar-setup">
+          <div id="date-info"> December 25 2024</div>
+          <div>
+            <FaCalendarDays className="calendar-icon" />
+          </div>
+          <div>
+            <div>
+              <GoChevronLeft id="left-nav" />
+              <GoChevronRight id="right-nav" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
